80451633d4c0177726e30622278a46179eb102c6
jxspray <66114831+jxspray@users.noreply.github.com>
2023-10-12 23:04:53 +0800

    前端存档

--- app/admin/model/cms/Content.php
+++ app/admin/model/cms/Content.php
@@ -3,19 +3,68 @@
 namespace app\admin\model\cms;
 
 
-class Content
+use think\Model;
+
+class Content extends Model
 {
-    public static function getInstance(string $name): object
+    public function __construct(string|null $name, object|array $data = [])
     {
-        return \think\facade\Db::name(\app\index\logics\CmsLogic::PREFIX . $name);
+        if ($name) $this->name = $name;
+        parent::__construct($data);
     }
 
-//    public static function onAfterWrite(self $model): void
-//    {
-//        // 设置url
-//        // 获取栏目目录
-//        $catalog = cms('catalog')[$model->catid];
-//        $model->url = $catalog['url'] . '/' . $model->id . '.html';
-//        $model->save();
-//    }
+    public static function getInstance(string $name): object
+    {
+        return new self(\app\index\logics\CmsLogic::PREFIX . $name, []);
+    }
+
+    public static function onBeforeWrite(self $model): void
+    {
+        // 设置url
+        // 获取栏目目录
+        $catalog = cms('catalog')[$model->catid];
+        // 检查是否启动路由模式
+        $module = cms("module")[$catalog['module_id']];
+        if ($module['rule']) {
+            $model->url = '/' . $module['rule'] . '/' . $model->id . '.html';
+        } else {
+            $model->url = $catalog['url'] . '/' . $model->id . '.html';
+        }
+    }
+
+
+
+    /**
+     * 创建新的模型实例.
+     *
+     * @param array $data    数据
+     * @param mixed $where   更新条件
+     * @param array $options 参数
+     *
+     * @return Model
+     */
+    public function newInstance(array $data = [], $where = null, array $options = []): Model
+    {
+        $model = new static($this->name, $data);
+
+        if ($this->connection) {
+            $model->setConnection($this->connection);
+        }
+
+        if ($this->suffix) {
+            $model->setSuffix($this->suffix);
+        }
+
+        if (empty($data)) {
+            return $model;
+        }
+
+        $model->exists(true);
+
+        $model->setUpdateWhere($where);
+
+        $model->trigger('AfterRead');
+
+        return $model;
+    }
 }
