f91b1736e132a183669dd1604acb2b87e5a753c4
jxspray <1532946322@qq.com>
2025-03-08 23:28:12 +0800

    refactor(preview): 更新数据库配置并移除安装相关文件
    - 在 .env-example 文件中添加了数据库配置
    - 更新了 buildadmin.php 中的加密密钥
    - 调整了 database.php 中的数据库连接参数
    - 删除了安装相关的 HTML 和 SVG 文件

--- config/buildadmin.php
+++ config/buildadmin.php
@@ -42,7 +42,7 @@ return [
         // 默认驱动方式
         'default' => 'mysql',
         // 加密key
-        'key'     => 'tcbDgmqLVzuAdNH39o0QnhOisvSCFZ7I',
+        'key'     => 'aMiTdx4pU8tlB52Ls1JuzhkDyefEgPjQ',
         // 加密方式
         'algo'    => 'ripemd160',
         // 驱动
